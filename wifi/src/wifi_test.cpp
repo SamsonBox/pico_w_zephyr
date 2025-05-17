6e0e7d186a7e1dbf5f6f0ecef8eff10d2cbe4dd1
SamsonBox <samson.box@gmx.net>
2025-05-17 19:01:49 +0200

    try official layout

--- wifi/src/wifi_test.cpp
+++ wifi/src/wifi_test.cpp
@@ -6,14 +6,18 @@
 
 #include <zephyr/kernel.h>
 #include <errno.h>
+#ifdef CONFIG_WIFI_CONNECT
 #include "wifi_connector.h"
+#endif
 
-
+#ifdef CONFIG_WIFI_CONNECT
 wificonnector::WifiAutoConnect* wificonnector::WifiAutoConnect::mInstance=nullptr;
-
+#endif
 int main(void)
 {
+#ifdef CONFIG_WIFI_CONNECT
 	wificonnector::WifiAutoConnect* autoConnect = wificonnector::WifiAutoConnect::get_instance();
 	autoConnect->connect();
+#endif
 	return 0;
 }
